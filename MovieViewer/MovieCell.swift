5c464bc482b7fe6441f46b5db45adf79cf770a98
Abu Butt <buttabu@yahoo.com>
2017-07-01 20:03:47 -0400

    First Commit

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Abu Butt on 7/1/17.
+//  Copyright © 2017 Abu Butt. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
